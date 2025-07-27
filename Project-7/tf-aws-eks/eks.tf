2daaff8a848377641d2a173d79d1d547fb984054
oezeakachi <oezeakac@redhat.com>
2025-07-27 04:50:21 +0100

    changes

--- Project-7/tf-aws-eks/eks.tf
+++ Project-7/tf-aws-eks/eks.tf
@@ -1,10 +1,39 @@
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "my-eks-cluster-control-plane-role" # Choose a descriptive name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      },
+    ]
+  })
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+# 2. Attach the AmazonEKSClusterPolicy to the role
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attach" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
 # Ref - https://registry.terraform.io/modules/terraform-aws-modules/eks/aws/latest
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.37.1"
   #map_users = var.aws_auth_users
-
+  # Pass the ARN of the newly created IAM role here
+  cluster_iam_role_arn = aws_iam_role.eks_cluster_role.arn 
   cluster_name    = "my-eks-cluster"
   cluster_version = "1.29"
 
