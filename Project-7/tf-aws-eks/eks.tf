1598abdc24c1e5c849a892e8ac9f4e5d66428650
oezeakachi <oezeakac@redhat.com>
2025-08-06 07:38:54 +0100

    moved Dockerfile around

--- Project-7/tf-aws-eks/eks.tf
+++ Project-7/tf-aws-eks/eks.tf
@@ -2,8 +2,7 @@
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "20.37.1"
-  #map_users = var.aws_auth_users
+  version = "~> 20.0"
 
   cluster_name    = "my-eks-cluster"
   cluster_version = "1.29"
